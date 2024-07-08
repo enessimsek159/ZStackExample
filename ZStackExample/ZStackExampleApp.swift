d06f8f7ccb0b5609f87f2db74d240fc11a424d53
Enes <47952155+enessimsek159@users.noreply.github.com>
2024-07-08 13:27:21 +0300

    Initial Commit

---
+++ ZStackExample/ZStackExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZStackExampleApp.swift
+//  ZStackExample
+//
+//  Created by Mustafa Şimşek on 8.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ZStackExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
